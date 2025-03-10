7813ed11c68508936b35f396544caf73be84fac2
HyunYoungMin <djfkfk12@naver.com>
2025-03-11 01:59:36 +0900

    1.통계 기능 추가

--- src/main/java/com/soloProject/myTrip/repository/MemberReservationRepository.java
+++ src/main/java/com/soloProject/myTrip/repository/MemberReservationRepository.java
@@ -4,6 +4,8 @@ import com.soloProject.myTrip.entity.Member;
 import com.soloProject.myTrip.entity.MemberReservation;
 import com.soloProject.myTrip.constant.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -21,4 +23,9 @@ public interface MemberReservationRepository extends JpaRepository<MemberReserva
             LocalDateTime dateTime);
 
     List<MemberReservation> findByItemReservationId(Long itemReservationId);
+
+    List<MemberReservation> findByReservedAtAfter(LocalDateTime dateTime);
+
+    @Query("SELECT COUNT(mr) FROM MemberReservation mr WHERE mr.reservedAt >= :startDate")
+    long countReservationsAfter(@Param("startDate") LocalDateTime startDate);
 }
--- src/main/java/com/soloProject/myTrip/repository/PaymentRepository.java
+++ src/main/java/com/soloProject/myTrip/repository/PaymentRepository.java
@@ -3,9 +3,13 @@ package com.soloProject.myTrip.repository;
 import com.soloProject.myTrip.constant.PaymentType;
 import com.soloProject.myTrip.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +20,9 @@ public interface PaymentRepository
         Payment findByMemberReservationIdAndPaymentType(Long memberReservationId, PaymentType paymentType);
 
         boolean existsByMemberReservationIdAndPaymentType(Long memberReservationId, PaymentType paymentType);
+
+        List<Payment> findByRegTimeAfter(LocalDateTime dateTime);
+        
+        @Query("SELECT SUM(p.amount) FROM Payment p WHERE p.regTime >= :startDate")
+        Integer sumAmountAfter(@Param("startDate") LocalDateTime startDate);
 }
